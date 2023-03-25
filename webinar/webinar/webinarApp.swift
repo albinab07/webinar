982344dc4180b3c6bb845052c9f064d025e91479
Albina Bratus <albinab07@gmail.com>
2023-03-25 22:58:16 +0200

    git

---
+++ webinar/webinar/webinarApp.swift
@@ -0,0 +1,17 @@
+//
+//  webinarApp.swift
+//  webinar
+//
+//  Created by 1 on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct webinarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
